bce0116e769aa5e1443472e5147ea2b68c580bf9
kuldeep2040 <kuldeepsinghks2040@gmail.com>
2025-07-09 01:04:34 +0530

    remove unnecessary allowFullScreen attribute from iframe in ContactUsPage

--- Client/src/pages/ContactUsPage.tsx
+++ Client/src/pages/ContactUsPage.tsx
@@ -140,7 +140,6 @@ export default function ContactUsPage() {
                   width="100%" 
                   height="100%" 
                   style={{border: 0}}
-                  allowFullScreen="" 
                   loading="lazy" 
                   referrerPolicy="no-referrer-when-downgrade"
                   title="IIIT Una Location"
